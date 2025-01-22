a64096adf5c2086aa04beb2e84fcab1971b0fc2b
roff-dev <kieronoates@hotmail.co.uk>
2025-01-22 18:01:10 +0000

    creating views

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,5 +6,26 @@
     </x-slot>
 
 
+    <div>
+        <h1>Overview</h1>
+        <div>
+            <h2>Total Companies</h2>
+            <p>number of companies</p>
+            <button>View Companies</button>
+        </div>
+        <div>
+            <h2>Total Employees</h2>
+            <p>number of employees</p>
+            <button>View Employees</button>
+        </div>
+    </div>
+
+    <div>
+        <h1>Highest Performing Companies</h1>
+        <div>card 1</div>
+        <div>card 2</div>
+        <div>card 3</div>
+    </div>
+
 </x-app-layout>
 
